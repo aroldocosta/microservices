72586d290107c90c6e3ce6dd80fc602c49b7a8c7
Aroldo Costa <aroldocosta@yahoo.com.br>
2022-11-26 17:01:15 -0300

    Primeiro commit

---
+++ 01-foo-bar-final/src/test/java/com/oficinabr/StartupTests.java
@@ -0,0 +1,13 @@
+package com.oficinabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
