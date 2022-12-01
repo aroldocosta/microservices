dd5f700cda63481d82f2d7634388f3ce72b46c08
Aroldo Costa <aroldocosta@yahoo.com.br>
2022-12-01 20:37:17 -0300

    Adicionada funcionalidade de obter a porta do serviço (environment)

--- 06-cambio-server/src/main/java/com/oficinabr/controller/CambioController.java
+++ 06-cambio-server/src/main/java/com/oficinabr/controller/CambioController.java
@@ -2,6 +2,8 @@ package com.oficinabr.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,18 @@ import com.oficinabr.model.Cambio;
 @RequestMapping("/cambio-service")
 public class CambioController {
 
+	@Autowired
+	private Environment environment;
+	
 	@GetMapping(value = "{amount}/{from}/{to}")
 	public Cambio getCambio(@PathVariable("amount") BigDecimal amount,
 			@PathVariable("from") String from,
 			@PathVariable("to") String to) {
+		
+		var port = environment.getProperty("local.server.port");
+		
 		return new Cambio(
-				1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+				1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
 	}
 
 }
