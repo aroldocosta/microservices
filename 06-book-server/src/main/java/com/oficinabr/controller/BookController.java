4f5495fe04344fe9b66be24f62af98061038e158
Aroldo Costa <aroldocosta@yahoo.com.br>
2022-12-14 21:59:21 -0300

    Modificado o nome da pasta do projeto/adicionada configuração do eureka

---
+++ 06-book-server/src/main/java/com/oficinabr/controller/BookController.java
@@ -0,0 +1,54 @@
+package com.oficinabr.controller;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.oficinabr.model.Book;
+import com.oficinabr.proxy.CambioProxy;
+import com.oficinabr.repository.BookRepository;
+import com.oficinabr.response.Cambio;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Autowired
+	private CambioProxy cambioProxy;
+	
+	@GetMapping(value = "/{id}/{currency}")
+	public Book findByIdAndCurrency(
+			@PathVariable("id") Long id,
+			@PathVariable("currency") String currency
+			) {
+		
+		String port = environment.getProperty("local.server.port");
+		
+		var book = bookRepository.findById(id).get();
+		
+		if( book == null ) throw new RuntimeException("Book not found");
+		
+		var cambio = cambioProxy.getCambio(book.getPrice(), "USD", currency);
+		
+		var price = cambio.getConvertedValue();
+
+		book.setPrice(price);
+		
+		book.setEnvironment(port);
+		
+		return book;
+
+	}
+}
