ba13b485ca677372d6963a231e0fee573e331b8f
Aroldo Costa <aroldocosta@yahoo.com.br>
2022-12-14 22:01:23 -0300

    Renomeada pasta do projeto

--- 07-book-server/src/main/java/com/oficinabr/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.oficinabr.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oficinabr.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {}
